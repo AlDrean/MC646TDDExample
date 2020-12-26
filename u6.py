94f059185b6214194cecae0cfb359e7e37a0f208
lucas <lucas.henrique.wakI@gmail.com>
2020-12-26 12:51:54 -0300

    joining 1 file, both test and devClass; Green for listFiles

--- u6.py
+++ u6.py
@@ -1,3 +1,17 @@
+
+from os import listdir
+from os.path import isfile, join
+
+class Program:
+
+    def __init__(self, directory):
+        self.directory = directory
+
+    def list_files(self):
+        onlyfiles = [f for f in listdir(self.directory) if isfile(join(self.directory, f))]
+        print (onlyfiles)
+        
+
 class test():
     def __init__(self,program):
         self.files_directory = 'files/'
@@ -7,5 +21,12 @@ class test():
         #Class for testing if listing all the files in the default directory;
         self.Program.list_files()
 
-    
+
+
+
+directory = 'files/'
+x = Program(directory)
+test_class = test(x)
+
+x.list_files()
     
