b38df5c2a28972c99284e483a8a946ce86d9c341
lucas <lucas.henrique.wakI@gmail.com>
2020-12-26 14:03:14 -0300

    green: test to open a file

--- u6.py
+++ u6.py
@@ -8,10 +8,20 @@ class Program:
         self.directory = directory
         self.file_List = []
         self.selectFile = -1
+        self.file_content = ""
 
     def list_files(self):
         self.file_List = [f for f in listdir(self.directory) if isfile(join(self.directory, f))]
         print (self.file_List)
+
+    def open_file(self,numFile):
+        self.selectFile = numFile
+        f = open(self.directory+self.file_List[self.selectFile], "r")
+        
+        print ("[OPENNING FILE] - {}".format(self.directory+self.file_List[self.selectFile]))
+        self.file_content = f.read()
+        f.close()
+        return self.file_content
         
 
 class test():
@@ -40,5 +50,6 @@ class test():
 directory = 'files/'
 x = Program(directory)
 test_class = test(x)
+test_class.test_listFiles()
 
 test_class.test_openFile(5)    
